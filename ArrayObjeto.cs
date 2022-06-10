c339b57775e7b775447967363145b37874e08a2d
catherine <83716701+catherine786@users.noreply.github.com>
2022-06-10 09:49:53 -0300

    Update ArrayObjeto.cs

--- ArrayObjeto.cs
+++ ArrayObjeto.cs
@@ -10,6 +10,57 @@ namespace ArrayObjeto
     {
         static void Main(string[] args)
         {
+            marzo[] objMarzo = new marzo[6];
+
+            objMarzo[0].id = 1;
+            objMarzo[0].Nombre = "once";
+
+            objMarzo[1].id = 2;
+            objMarzo[1].Nombre = "San Nicolás";
+
+            objMarzo[2].id = 3;
+            objMarzo[2].Nombre = "Almagro";
+
+            objMarzo[3].id = 4;
+            objMarzo[3].Nombre = "Microcentro";
+
+            objMarzo[4].id = 5;
+            objMarzo[4].Nombre = "Microcentro";
+
+            objMarzo[5].id = 6;
+            objMarzo[5].Nombre = "Microcentro";
+
+            objMarzo[6].id = 7;
+            objMarzo[6].Nombre = "Microcentro";
+
+
+        }
+        public class marzo
+        {
+            public marzo() { }
+
+            public int id;
+            public string Nombre;
+            
+            public int NumeroDeSucursal
+            {
+
+            }
+        
+            public int MontoDeVenta
+            {
+
+            }
+
+            public int CantidadDeEmpleados
+            {
+
+            }
+
+            public int CantDeClientesDelMes
+            {
+
+            }
         }
     }
 }
