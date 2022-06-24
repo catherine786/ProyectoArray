b6765e26d3230971c1a763713750b72ec1b22ce3
catherine <83716701+catherine786@users.noreply.github.com>
2022-06-24 11:59:01 -0300

    Update ArrayObjeto.cs

--- ArrayObjeto.cs
+++ ArrayObjeto.cs
@@ -10,39 +10,34 @@ namespace ArrayObjeto
     {
         static void Main(string[] args)
         {
-            
-            Console.WriteLine("Ingresar la cantidad de sucursales");
-            int cantidadSucursales = Convert.ToInt32(Console.ReadLine());
-            int[] MontoDeVentas = new int[cantidadSucursales];
-            int[] CantidadDeEmpleados = new int[cantidadSucursales];
-            int[] CantClientesMes = new int[cantidadSucursales];
-            int sumaVentas = 0;
-
-            for (int i = 0; i < cantidadSucursales; i++)
+            sucursales[] objSucursales = new sucursales[7];
+            //Pregunto de ctas sucursales va a ingresar datos
+            Console.WriteLine("Ingrese de cuantas sucursales va a ingresar datos:");
+            int cantSuc = Convert.ToInt32(Console.ReadLine());
+            for (int i=0; i < cantSuc; i++)
             {
-                Console.WriteLine("Ingrese los datos de la sucursal {0}", i + 1);
+                Console.WriteLine("Ingrese el numero de la sucursal: ");
+                int numS = Convert.ToInt32(Console.ReadLine());
+
+                objSucursales[numS] = new sucursales();
+
                 Console.WriteLine("Ingresar el monto de venta: ");
-                MontoDeVentas[i] = Convert.ToInt32(Console.ReadLine());
+                objSucursales[numS].MontoDeVenta = Convert.ToDecimal(Console.ReadLine());
                 Console.WriteLine("Ingresar la cantidad de empleados: ");
-                CantidadDeEmpleados[i] = Convert.ToInt32(Console.ReadLine());
+                objSucursales[numS].CantidadDeEmpleados = Convert.ToInt32(Console.ReadLine());
                 Console.WriteLine("Ingresar la cantidad de clientes que compraron algo este mes: ");
-                CantClientesMes[i] = Convert.ToInt32(Console.ReadLine());
-
-                sumaVentas += MontoDeVentas[i];
+                objSucursales[numS].CantClientesMes = Convert.ToInt32(Console.ReadLine());
+                
             }
-            Console.WriteLine(sumaVentas);
+
+            
 
 
-            //For de prueba
-            /*
-            for (int i = 0; i < cantidadSucursales; i++)
-            {
-                Console.WriteLine("_________________");
-                Console.WriteLine(MontoDeVentas[i]);
-                Console.WriteLine(CantidadDeEmpleados[i]);
-                Console.WriteLine(CantClientesMes[i]);
-            }
-            */
+            string[] NombreSucursal = new string[7] { "Once", "San Nicolas", "Almagro", "Microcentro", "Palermo", "San Cristobal", "Parque Patricios" };
+
+
+
+            
             Console.ReadKey();
         }
         public class sucursales
@@ -52,19 +47,38 @@ namespace ArrayObjeto
             {
 
             }
-            //Atributos
 
-            int cantidadSucursales;
-            
+            //Atributos
+            public decimal MontoDeVenta = 0;
+            public int CantidadDeEmpleados = 0;
+            public int CantClientesMes = 0;
+
             //Metodos
-            public void MostrarDatos()
+            
+
+            public void PromedioVentas( int sumVt, int cantSuc )
             {
-                Console.WriteLine("La sucursal ");
+                Console.WriteLine("El promedio de ventas es de: {0} ", sumVt / cantSuc);
             }
 
-            public void PromedioVentas()
+            public void PromedioEmpleados(int sumEmp, int cantSuc)
             {
-                Console.WriteLine(sumaVentas / cantidadSucursales);
+                Console.WriteLine("El promedio de empleados es de: {0} ", sumEmp / cantSuc);
+            }
+
+
+        }
+        
+        public static class calculos
+        {
+            //metodos
+            public static int MostrarDatos(sucursales[] arreglo)
+            {
+                for (int i = 0; i < 7; i++)
+                {
+                    Console.WriteLine("Monto de venta {0}, Emp {1}, Clientes {2}", arreglo[i].MontoDeVenta, arreglo[i].CantidadDeEmpleados, arreglo[i].CantClientesMes);
+                }
+                    
             }
         }
     }
