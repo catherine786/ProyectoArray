75a708b2fc6c42bf752bf404f04e662f901a0783
Aclaem <83714656+Aclaem@users.noreply.github.com>
2022-06-30 10:41:29 -0300

    Comentarios en el archivo para entender mejor

--- EjemploArrayNombresEdades.cs
+++ EjemploArrayNombresEdades.cs
@@ -15,7 +15,7 @@ namespace sec
             int[] Numeros = new int[4] {1,2,3,4};
             int[] Edades = new int[4];
 
-            for (int i = 0; i < 4; i++)
+            for (int i = 0; i < 4; i++) //Este for es solo para mostrar los nombres y numeros antes de comenzar a pedir datos, para que el usuario sepa cual corresponde a cual.
             {
                 Console.WriteLine("{0}  {1}", (i+1), Nombres[i]);
             }
@@ -24,7 +24,7 @@ namespace sec
             Console.WriteLine("Cuantas edades va a ingresar?");
             int cantEdades = Convert.ToInt32(Console.ReadLine());
 
-            for (int i = 0; i < cantEdades; i++)
+            for (int i = 0; i < cantEdades; i++) //Este for pide los datos y se ejecuta tantas veces como haya dicho el usuario.
             {
                 Console.WriteLine("De que alumno quiere ingresar la edad?");
                 int numAlumno = (Convert.ToInt32(Console.ReadLine()) - 1);
@@ -33,7 +33,7 @@ namespace sec
                 Edades[numAlumno] = Convert.ToInt32(Console.ReadLine());
             }
 
-            for (int i =0; i < 4; i++)
+            for (int i =0; i < 4; i++)// Por ultimo, este for sirve para mostrar los datos de TODOS los alumnos, tanto los que tienen edades ingresadas como los que no.
             {
                 Console.WriteLine("Nombre: {0}, edad: {1}", Nombres[i], Edades[i]);
             }
@@ -44,7 +44,8 @@ namespace sec
     {
         public Persona() { }
 
-        public  string[] Nombres = new string[4] { "Cathy", "Axel", "Facu", "Ale" };
+        //Creo que estas dos lineas no hacen absolutamente nada
+        public  string[] Nombres = new string[4] { "Cathy", "Axel", "Facu", "Ale" }; 
         public int[] Edades = new int[4];
     }
 }
