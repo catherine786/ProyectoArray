f75e57d8bf0f138efd35812fc2bb50cbf2969557
catherine <83716701+catherine786@users.noreply.github.com>
2022-07-01 10:17:39 -0300

    Update ArrayObjeto.cs

--- ArrayObjeto.cs
+++ ArrayObjeto.cs
@@ -13,36 +13,44 @@ namespace Consola02
             // instanciacion de la clase
             sucursal[] objSucursal = new sucursal[7];
             string[] NombreSucursal = new string[] { "Once", "San Nicolas", "Almagro", "Microcentro", "Palermo", "San Cristobal", "Parque Patricios" };
+            
             Console.WriteLine("Sucursales:");
             for (int i = 0; i < 7; i++)
             {
                 Console.WriteLine("{0}  {1}", (i + 1), NombreSucursal[i]);
             }
 
-            for (int i = 0; i < objSucursal.Length; i++)
+            Console.WriteLine("Cuantas sucursales va a ingresar?");
+            int cantSuc = Convert.ToInt32(Console.ReadLine());
+            int[] sucIn = new int[cantSuc];
+
+            for (int i = 0; i < cantSuc; i++)
             {
                 Console.WriteLine("De que Sucursal va a ingresar datos?");
-                int n = (Convert.ToInt32(Console.ReadLine()) - 1);
-                objSucursal[n] = new sucursal();
-                Console.WriteLine("Esta por ingresar los datos de la sucursal de: {0}", NombreSucursal[n]);
-                objSucursal[n].NumeroDeSucursal= i;
+                int numSuc = (Convert.ToInt32(Console.ReadLine()) - 1);
+                
+                sucIn[i] = numSuc;
+                objSucursal[numSuc] = new sucursal();
+                Console.WriteLine("Esta por ingresar los datos de la sucursal de: {0}", NombreSucursal[numSuc]);
                 Console.WriteLine("Ingresar el monto de venta del mes");
-                objSucursal[n].MontoDeVenta = Convert.ToDecimal(Console.ReadLine());
+                objSucursal[numSuc].MontoDeVenta = Convert.ToDecimal(Console.ReadLine());
                 Console.WriteLine("Ingresar la cantidad de empleados: ");
-                objSucursal[n].CantDeEmpleados = Convert.ToInt32(Console.ReadLine());
+                objSucursal[numSuc].CantDeEmpleados = Convert.ToInt32(Console.ReadLine());
                 Console.WriteLine("Ingresar la cantidad de clientes que compraron algo este mes: ");
-                objSucursal[n].CantDeClientes = Convert.ToInt32(Console.ReadLine());
+                objSucursal[numSuc].CantDeClientes = Convert.ToInt32(Console.ReadLine());
+
+                objSucursal[numSuc].NumeroDeSucursal = numSuc;
             }
- 
+
 
             // salida de informacion 
             // calculo del promedio de la edad
-
-            Calcular.Datos(NombreSucursal, objSucursal);
-            Calcular.calcPromVenta(objSucursal);
-            Calcular.VentasPorEmpleado(NombreSucursal, objSucursal);
-            Calcular.VentaMax(NombreSucursal, objSucursal);
-            Calcular.VentaMin(NombreSucursal, objSucursal);
+            Calcular.Datos(sucIn, NombreSucursal, objSucursal);
+            Calcular.calcPromVenta(sucIn, objSucursal);
+            Calcular.calcPromEmple(sucIn, objSucursal);
+            Calcular.VentasPorEmpleado(sucIn, NombreSucursal, objSucursal);
+            Calcular.VentaMax(sucIn, NombreSucursal, objSucursal);
+            Calcular.VentaMin(sucIn, NombreSucursal, objSucursal);
 
         }
     }
@@ -50,7 +58,7 @@ namespace Consola02
     {
 
         // constructor
-        public sucursal(){}
+        public sucursal() { }
 
         // atributos
         public int NumeroDeSucursal;
@@ -60,54 +68,68 @@ namespace Consola02
     }
     public static class Calcular
     {
-        
-        public static int Datos(string[] NombreSucursal, sucursal[] objSuc)
+        public static int Datos(int[] sucIn,string[] NombreSucursal, sucursal[] objSuc)
         {
-            for (int i = 0; i < objSuc.Length; i++)
+            for (int i = 0; i < sucIn.Length; i++)
             {
-                Console.WriteLine("Datos de la sucursal de {0} \n El monto de venta fue de: ${1} Tienen una cantidad de: {2} empleados y {3} clientes compraron este mes", NombreSucursal[i], objSuc[i].MontoDeVenta, objSuc[i].CantDeEmpleados, objSuc[i].CantDeClientes);
+                Console.WriteLine("{0} El monto de venta fue de: ${1} Tienen una cantidad de: {2} empleados y {3} clientes compraron este mes",NombreSucursal[sucIn[i]], objSuc[sucIn[i]].MontoDeVenta, objSuc[sucIn[i]].CantDeEmpleados, objSuc[sucIn[i]].CantDeClientes);
             }
             return 0;
         }
 
-        public static int calcPromVenta(sucursal[] objSuc)
+        public static int calcPromEmple(int[] sucIn, sucursal[] objSuc)
+        {
+            int sumatoria = 0;
+            for (int i = 0; i < sucIn.Length; i++)
+            {
+                sumatoria = sumatoria + objSuc[sucIn[i]].CantDeEmpleados;
+            }
+            int promedio = sumatoria / sucIn.Length;
+            Console.WriteLine("La suma de emmpleados es de: {0} \n El promedio de Empleados de todas las sucursales fue de: {1}", sumatoria, promedio);
+            return 0;
+        }
+
+
+        public static int calcPromVenta(int[] sucIn, sucursal[] objSuc)
         {
             decimal promedio = 0;
             decimal sumatoria = 0;
-            for (int i = 0; i < objSuc.Length; i++)
+            for (int i = 0; i < sucIn.Length; i++)
             {
-                sumatoria = sumatoria + objSuc[i].MontoDeVenta;
+                sumatoria = sumatoria + objSuc[sucIn[i]].MontoDeVenta;
             }
-            promedio = sumatoria / objSuc.Length;
-            Console.WriteLine("El promedio de venta de todas las sucursales fue de: ${0} este mes", promedio);
+            promedio = sumatoria / sucIn.Length;
+            Console.WriteLine("La suma de venta es de: {0} \n El promedio de venta de todas las sucursales fue de: ${1:N2} este mes", sumatoria, promedio);
             return 0;
         }
-        public static int VentasPorEmpleado(string[] NombreSucursal, sucursal[] objSuc)
+
+        public static int VentasPorEmpleado(int[] sucIn, string[] NombreSucursal, sucursal[] objSuc)
         {
-            int[] PromPorEmple = new int[objSuc.Length];
-            int promedio = 0;
-            for (int i = 0; i < objSuc.Length; i++)
+            int suma1 = 0;
+            decimal suma2 = 0;
+            for (int i = 0; i < sucIn.Length; i++)
             {
-                int emple= objSuc[i].CantDeEmpleados;
-                int ventas= objSuc[i].CantDeClientes;
-                promedio = ventas / emple;
-                PromPorEmple[i]= promedio;
-                Console.WriteLine("En la sucursal de {0} tuvieron un promedio de {1} ventas por empleado", NombreSucursal[i] , PromPorEmple[i]);
+                suma1 = suma1 + objSuc[sucIn[i]].CantDeEmpleados;
+                suma2 = suma2 + objSuc[sucIn[i]].MontoDeVenta;
+
+
             }
+            decimal promedio = suma2 / suma1;
+            Console.WriteLine("El promedio de venta por empleado es de: {0:N2}", promedio);
             
             return 0;
         }
-        public static int VentaMax(string[] NombreSucursal, sucursal[] objSuc)
+        public static int VentaMax(int[] sucIn, string[] NombreSucursal, sucursal[] objSuc)
         {
-            decimal VentaMaxima = objSuc[0].MontoDeVenta;
-            int n = objSuc[0].NumeroDeSucursal;
-            for (int i = 0; i < objSuc.Length; i++)
+            decimal VentaMaxima = objSuc[sucIn[0]].MontoDeVenta;
+            int n = objSuc[sucIn[0]].NumeroDeSucursal;
+            for (int i = 0; i < sucIn.Length; i++)
             {
-                if (VentaMaxima < objSuc[i].MontoDeVenta)
+                if (VentaMaxima < objSuc[sucIn[i]].MontoDeVenta)
                 {
-                    VentaMaxima = objSuc[i].MontoDeVenta;
-                     n = objSuc[i].NumeroDeSucursal;
-                    
+                    VentaMaxima = objSuc[sucIn[i]].MontoDeVenta;
+                    n = objSuc[sucIn[i]].NumeroDeSucursal;
+
                 }
             }
             Console.WriteLine("la venta maxima le pertenese a la sucursal de {0} y fue de: ${1}", NombreSucursal[n], VentaMaxima);
@@ -116,16 +138,16 @@ namespace Consola02
             return 0;
 
         }
-        public static int VentaMin(string[] NombreSucursal, sucursal[] objSuc)
+        public static int VentaMin(int[] sucIn, string[] NombreSucursal, sucursal[] objSuc)
         {
-            decimal VentaMinima = objSuc[0].MontoDeVenta;
-            int n= objSuc[0].NumeroDeSucursal;
-            for (int i = 0; i < objSuc.Length; i++)
+            decimal VentaMinima = objSuc[sucIn[0]].MontoDeVenta;
+            int n = objSuc[sucIn[0]].NumeroDeSucursal;
+            for (int i = 0; i < sucIn.Length; i++)
             {
-                if (VentaMinima > objSuc[i].MontoDeVenta)
+                if (VentaMinima > objSuc[sucIn[i]].MontoDeVenta)
                 {
-                    VentaMinima = objSuc[i].MontoDeVenta;
-                    n = objSuc[i].NumeroDeSucursal;
+                    VentaMinima = objSuc[sucIn[i]].MontoDeVenta;
+                    n = objSuc[sucIn[i]].NumeroDeSucursal;
 
                 }
 
